8f491c92ace7fe06a006f20fac0fcfc1675ed7a8
SahibChy <sc22aby@herts.ac.uk>
2025-01-06 02:45:52 +0000

    Log class

---
+++ src/main/sahib/java/model/Log.java
@@ -0,0 +1,34 @@
+package main.sahib.java.model;
+
+import java.io.FileWriter;
+import java.io.IOException;
+
+public class Log {
+    private static Log instance = null;
+    private StringBuilder logData;
+
+    private Log() {
+        logData = new StringBuilder();
+    }
+
+    public static Log getInstance() {
+        if (instance == null) {
+            instance = new Log();
+        }
+        return instance;
+    }
+
+    public void addEntry(String entry) {
+        logData.append(entry).append("\n");
+    }
+
+    public void saveLog(String filename) throws IOException {
+        FileWriter writer = new FileWriter(filename);
+        writer.write(logData.toString());
+        writer.close();
+    }
+
+    public String getLogs() {
+        return logData.toString();
+    }
+}
